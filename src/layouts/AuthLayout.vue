cb220620a97cd10d39367a9a1f48ec49b6be80ed
中月不吃辣 <lizhongyue248@163.com>
2022-10-07 16:16:05 +0800

    💄 Add Login page.

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
-#auth-container.flex.items-center.flex-col.justify-center
-  .auht-logo.text-h2.text-white.logo.text-stroke-sm.q-my-lg(class="<sm:hidden") Lever
-  q-card.max-w-lg.full-width.shadow-18.animated.fadeIn.transition-all.q-mb-xl(class="<sm:(h-screen w-full max-w-none mb-0)")
-    q-card-section.q-ma-md
-      router-view(v-slot="{ Component }")
-        transition(enter-active-class="animated fadeIn", leave-active-class="animated fadeOut", appear, mode="out-in" )
-          component(:is="Component")
+#auth.flex.items-center.flex-col
+  .row.no-wrap.flex.items-center.full-width.text-white.px-8.py-4
+    #auth-logo.font-bold Lever
+    q-space
+    #auth-tip Doesn't have an account?
+  #auth-container.justify-center.items-center.flex-grow.flex
+    q-card.full-width.shadow-18.animated.fadeIn.transition-all.q-mb-xl(class="<sm:(h-screen w-full max-w-none mb-0)")
+      q-card-section.q-pa-none
+        router-view(v-slot="{ Component }")
+          transition(enter-active-class="animated fadeIn", leave-active-class="animated fadeOut", appear, mode="out-in" )
+            component(:is="Component")
 </template>
 
 <script lang="ts" setup>
@@ -13,13 +17,16 @@
 </script>
 
 <style lang="scss" scoped>
-#auth-container {
+#auth {
   height: 100vh;
   width: 100%;
   background: url("src/assets/bg.jpg") no-repeat center center;
   background-size: cover;
   #auth-title {
     box-shadow: 0 8px 10px -5px rgba(0, 0, 0, 0.2), 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12);
+  }
+  #auth-container {
+    width: 996px;
   }
 }
 </style>
--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,8 +1,37 @@
 <template lang="pug">
-#login Login Page
+#login.flex.flex-row.flex-nowrap
+  #login-form.flex.justify-center.my-10(class="w-3/5")
+    .text-h4.text-weight-bold Sign in
+    #login-form-content.w-md.px-10.m-10
+      q-input.mb-6(filled, v-model="form.username", label="Username", placeholder="Enter Username.", stack-label)
+      q-input.mb-6(filled, v-model="form.password", label="Password", placeholder="Enter Password.", stack-label, :type="showPassword ? 'password' : 'text'")
+        template(v-slot:append)
+          q-icon.cursor-pointer(:name="showPassword ? 'visibility_off' : 'visibility'", @click="showPassword = !showPassword")
+      .row.mb-6
+        q-checkbox(v-model="form.remember", label="Remember me", :true-value="true", :false-value="false", dense)
+        q-space
+        a(href="https://www.baidu.com") Forget password?
+      q-btn.full-width(color="primary", label="Sign in", size="md")
+  #login-graph.bg-gray-500.flex.justify-center.items-center(class="w-2/5")
+    img(:src="Login")
 </template>
 
 <script lang="ts" setup>
+import Login from 'assets/login.svg'
+import { reactive, ref } from 'vue'
+
+interface LoginUser {
+  username: string;
+  password: string;
+  remember: boolean;
+}
+
+const form = reactive<LoginUser>({
+  username: '',
+  password: '',
+  remember: false
+})
+const showPassword = ref<boolean>(true)
 
 </script>
 
